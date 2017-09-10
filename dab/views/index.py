13209cab3c142fee0307e8302f9121113af14db3
John Robson <John.Robson@USP.br>
2017-09-10 02:51:05 -0400

    Reading CSV file

--- dab/views/index.py
+++ dab/views/index.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from flask import Blueprint, render_template
 
 index = Blueprint('index', __name__)
@@ -5,4 +7,9 @@ index = Blueprint('index', __name__)
 
 @index.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    filename = 'advertising'
+
+    df = pd.read_csv('/tmp/{}.csv'.format(filename))
+
+
+    return render_template('index.html', df=df)
