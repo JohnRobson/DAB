37a87ddf4bf81cd3ce50e41f46599805802d8cab
John Robson <John.Robson@USP.br>
2017-09-10 22:53:41 -0400

    List datasets in datasets page

--- dab/views/datasets.py
+++ dab/views/datasets.py
@@ -1,8 +1,18 @@
+from os import listdir
+from os.path import isfile, join
+
 from flask import Blueprint, render_template
+
+db_dir = 'db'
 
 datasets = Blueprint('datasets', __name__)
 
 
 @datasets.route('/datasets/', methods=['GET', 'POST'])
 def main():
-    return render_template('datasets.html')
+
+    datasets_files = [f.replace('.csv','') for f in listdir(db_dir) if isfile(join(db_dir, f)) and '.csv' in f] # get all directory elements, keep only CSV files and remove '.csv'
+
+    datasets_files.sort() # sort files names
+
+    return render_template('datasets.html', ds=datasets_files)
