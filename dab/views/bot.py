196b3ec987af686384d4766ff422da6b7c72b776
John Robson <John.Robson@USP.br>
2017-09-12 08:50:37 -0400

    Bot page - insert image

--- dab/views/bot.py
+++ dab/views/bot.py
@@ -1,9 +1,23 @@
-from collections import namedtuple
 import time
+from collections import namedtuple
+from io import BytesIO
+import numpy as np
 import pandas as pd
-from flask import Blueprint, render_template
+import matplotlib.pyplot as plt
+import prettyplotlib as ppl
+from flask import Blueprint, render_template, send_file  # , make_response
 
 bot = Blueprint('bot', __name__)
+
+
+@bot.route('/img/<img>')
+def img(img):
+	fig, ax = plt.subplots(1)
+	ppl.bar(ax, np.arange(10), np.abs(np.random.randn(10)))
+	img = BytesIO()
+	fig.savefig(img, format='png', dpi=72)
+	img.seek(0)
+	return send_file(img, mimetype='image/png')
 
 
 @bot.route('/bot/', methods=['GET', 'POST'])
@@ -20,5 +34,8 @@ def main():
 
 	df = pd.read_csv('db/{}.csv'.format(filename))  # read dataset file
 
+
 	nt = ntm(rt, error, forml, formf, run, df)
 	return render_template(nt.rt, nt=nt)
+
+
